9ddb8e8ade7c78bf15f274d92741fbd8a50cbe7f
Ivan javier <ig6119838@gmail.com>
2024-05-30 03:03:39 -0300

    check final

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll('.scroll-element');
+    const elements = document.querySelectorAll('.zoom-element');
 
     elements.forEach(element => {
-        element.classList.add('visible');
+        if (element.getBoundingClientRect().top < window.innerHeight) {
+            element.classList.add('visible');
+        }
     });
-})
+
+    window.addEventListener('scroll', () => {
+        elements.forEach(element => {
+            if (element.getBoundingClientRect().top < window.innerHeight) {
+                element.classList.add('visible');
+            }
+        });
+    });
+});
--- logeado/creaTublog.js
+++ logeado/creaTublog.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const elements = document.querySelectorAll('.scroll-element');
 
     elements.forEach(element => {
-        element.classList.add('visible');
+        if (element.getBoundingClientRect().top < window.innerHeight) {
+            element.classList.add('visible');
+        }
     });
-})
+
+    window.addEventListener('scroll', () => {
+        elements.forEach(element => {
+            if (element.getBoundingClientRect().top < window.innerHeight) {
+                element.classList.add('visible');
+            }
+        });
+    });
+});
